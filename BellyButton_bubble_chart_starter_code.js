ab8076dff5a37ccea40145a0810c17eb7d8f7839
greensleeves8 <theonlycalegreen@gmail.com>
2020-12-17 15:13:05 -0600

    github pages deploy

--- BellyButton_bubble_chart_starter_code.js
+++ BellyButton_bubble_chart_starter_code.js
@@ -53,39 +53,48 @@ function buildMetadata(sample) {
   });
 }
 
-// 1. Create the buildCharts function.
+// Bar and Bubble charts
+// Create the buildCharts function.
 function buildCharts(sample) {
-  // 2. Use d3.json to load and retrieve the samples.json file 
+  // Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
-    // 3. Create a variable that holds the samples array. 
-    var sampleData = data.samples; 
-    // 4. Create a variable that filters the samples for the object with the desired sample number.
-    var sampleArray = sampleData.filter(sampleObj => sampleObj.id == sample);
-    
-    //  5. Create a variable that holds the first sample in the array.
-    var sampleNumber = sampleArray[0]; 
+    // Create a variable that holds the samples array. 
 
-    // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-    var otuID = sampleNumber.otu_ids;
-    var otuLabel = sampleNumber.otu_labels;
-    var sampleValues = sampleNumber.sample_values;
-    console.log(otuID.slice(0,10)) 
+    // Create a variable that filters the samples for the object with the desired sample number.
 
-    // 7. Create the yticks for the bar chart.
+    // Create a variable that holds the first sample in the array.
+
+
+    // Create variables that hold the otu_ids, otu_labels, and sample_values.
+
+
+    // Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
-    //  so the otu_ids with the most bacteria are last. 
+    // so the otu_ids with the most bacteria are last. 
 
-    var yticks = otuID.sort((a, b) => sampleValues[a] - sampleValues[b]).slice(0, 10)
-    console.log(yticks)
-    // 8. Create the trace for the bar chart. 
-    // var barData = [
+    var yticks = 
+
+    // Create the trace for the bar chart. 
+    var barData = [
       
-    // ];
-    // 9. Create the layout for the bar chart. 
-    // var barLayout = {
+    ];
+    // Create the layout for the bar chart. 
+    var barLayout = {
      
-    // };
-    // 10. Use Plotly to plot the data with the layout. 
+    };
+    // Use Plotly to plot the data with the layout. 
+
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = [
+   
+    ];
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
     
   });
 }
